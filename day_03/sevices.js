a617e1af1e0c278aa3f293ed297635a9282deead
ayanpandit <aayanpandey8528@gmail.com>
2026-02-03 12:00:00 +0530

    day 01

--- day_03/sevices.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs/promises";
-export const fileStatus = async (path) => {
-    try {
-        const stats = await fs.stat(path);
-        return stats;
-    } catch (error) {
-       console.log("unable to find status");       
-    }
-}
