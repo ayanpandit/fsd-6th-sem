3a66445857a743ba79a9030e6de0a4a64adeb604
ayanpandit <aayanpandey8528@gmail.com>
2026-02-13 12:00:00 +0530

    day 04

---
+++ day_03/index.js
@@ -0,0 +1,12 @@
+import { fileStatus } from "./sevices.js";
+const useFileStats = async(path)=>{
+   const stats = await fileStatus(path);
+   console.log(stats.size); 
+   console.log(stats.isFile());
+   console.log(stats.isDirectory());
+}
+useFileStats("./sevices.js")
+// const stats = await fileStatus("./sevices.js");
+// console.log(stats.size); 
+// console.log(stats.isFile());
+// console.log(stats.isDirectory());
